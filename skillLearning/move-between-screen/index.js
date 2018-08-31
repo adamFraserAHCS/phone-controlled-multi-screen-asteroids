12f072fd847409abd3a4e14162ddba3454d63bf2
Adam <AdamFraserPrograming@gmail.com>
2018-08-31 20:08:08 +0100

    HO-HO-HO ly shit it actually works!

--- skillLearning/move-between-screen/index.js
+++ skillLearning/move-between-screen/index.js
@@ -4,60 +4,54 @@ var http = require('http').Server(app); //why?
 var path = require('path');
 var io = require('socket.io')(http);
 
-app.get('/1', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'arena/arena.html'));
 });
 
-app.get('/2', function(req, res) {
-    res.sendFile(path.join(__dirname, 'arena/arena2.html'));
-});
-
-var arena = io.of('/arenaSpace');
-var arenaRight = io.of('/arenaSpaceRight');
+var arenaSpace = io.of('/arenaSpace');
 
 var displays = [];
+var ballSent = false;
+arenaSpace.on('connection', function(socket) {
+    displays.push({id: socket.id, socket: socket});
 
-arena.on('connection', function(socket) {
-
-    displays.push(socket.id);
-
-    // socket.emit('recieve entity', {isLeft: false, y:250, vX:9});
-
-    socket.on('ooh pick me!', function(data) {
-        socket.emit('recieve entity', {isLeft: true, y: 250, vX: 12});
-    })
+    if(!ballSent) {
+        socket.emit('recieve entity', {isLeft: false, y:250, vY: 0, vX:-15});
+        ballSent = true;
+    }
 
     socket.on('switchScreens', function(data){
         var sendTo = getNextScreen(socket.id, data.isLeft);
-        console.log(sendTo);
-        var data2Send = {
-            'isLeft': data.isLeft,
-            'vX': data.vX,
-            'vY': data.vY,
-            'y': data.y
+        var dataToSend = {
+            isLeft: !data.isLeft,
+            y: data.y,
+            vX: data.vX,
+            vY: data.vY
         }
-        arena.emit('recieve entity', {isLeft: data.isLeft, y: data.y, vX: data.vX});
+
+        // arenaSpace.emit('recieve entity', {isLeft: !data.isLeft, y: data.y, vX: data.vX});
+        sendTo.emit('recieve entity', dataToSend);
     })
 });
 
 function getNextScreen(currentScrID, isLeft) {
     for(var i = 0; i < displays.length; i++) {
-        if(displays[i] == currentScrID) { break; }
+        if(displays[i].id == currentScrID) { break; }
     }
 
     // if the asteroid is coming from the left
     //of one screen to the right of annother
     if(isLeft) {
         if(i == 0) {
-            return displays[displays.length - 1];
+            return displays[displays.length - 1].socket;
         } else {
-            return displays[i - 1];
+            return displays[i - 1].socket;
         }
     } else {
         if(i == displays.length - 1) {
-            return displays[0];
+            return displays[0].socket;
         } else {
-            return displays[i + 1];
+            return displays[i + 1].socket;
         }
     }
 }
@@ -65,3 +59,5 @@ function getNextScreen(currentScrID, isLeft) {
 http.listen(3000, '0.0.0.0', function() {
     console.log('listening on *:3000');
 });
+
+//IDEA ADD OFFSET? e.g. one screen starts at y = 0 annother at y = 500
